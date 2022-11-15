ac6c007c316308ae3279ab75cd804496884477a5
sumanth <sunnyuk116@gmail.com>
2022-11-15 19:25:56 +0000

    cluster , node ,key , secrit

---
+++ modules/aks/aks_main.tf
@@ -0,0 +1,20 @@
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.aks_name
+  location            = var.location
+  resource_group_name = var.rg_name
+  dns_prefix          = "sumanthakss"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 2
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Production"
+  }
+}
